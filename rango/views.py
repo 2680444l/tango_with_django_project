54c9b116f91c4bebc5da59ccd95fa1b44545480a
2680444l <2680444l@student.gla.ac.uk>
2022-02-22 15:58:39 +0000

    Chapter6.2

--- rango/views.py
+++ rango/views.py
@@ -1,12 +1,25 @@
+from multiprocessing import context
 from django.shortcuts import render
+
+# Import the Category model from models.py
+from rango.models import Category
+from rango.models import Page
 
 # Create your views here.
 from django.http import HttpResponse
 
 # created a view called index
+# this is for the main page view
 def index(request):
-    # Refer to the TwD book for more information on how this updated view works.
-    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    # retrieve the top 5 likes in descending order
+    # pass a reference to the list
+    category_list = Category.objects.order_by('-likes')[:5]
+
+    context_dict = {}
+    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
+    context_dict['categories'] = category_list
+
+    # render the response and send it
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
