edd5a97946d6e4d120479bd8972b97a17c9a43b0
2680444l <2680444l@student.gla.ac.uk>
2022-02-22 12:45:57 +0000

    Chapter5_Exercises

--- rango/admin.py
+++ rango/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from rango.models import Category, Page
 
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
+
 # Register your models here.
 # these will be accessible via the web-based admin interface
 admin.site.register(Category)
-admin.site.register(Page)
+admin.site.register(Page, PageAdmin)
+
--- rango/models.py
+++ rango/models.py
@@ -5,6 +5,8 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
     # when unique=True, it can be a primary key
 
     # solve the typo issue
