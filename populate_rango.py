64167305ca58ae4b302eed18a8bf8def01412fdf
2680444l <2680444l@student.gla.ac.uk>
2022-02-22 12:21:08 +0000

    Chapter5

---
+++ populate_rango.py
@@ -0,0 +1,68 @@
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tango_with_django_project.settings')
+
+import django
+# import django project's settings
+django.setup()
+# import after the settings (django.setup())
+from rango.models import Category, Page
+
+# add category or pages here
+def populate():
+    python_pages = [
+        {'title': 'Official Python Tutorial',
+         'url':'http://docs.python.org/3/tutorial/'},
+        {'title':'How to Think like a Computer Scientist',
+         'url':'http://www.greenteapress.com/thinkpython/'},
+        {'title':'Learn Python in 10 Minutes',
+         'url':'http://www.korokithakis.net/tutorials/python/'} ]
+    
+    django_pages = [
+        {'title':'Official Django Tutorial',
+         'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/'},
+        {'title':'Django Rocks',
+         'url':'http://www.djangorocks.com/'},
+        {'title':'How to Tango with Django',
+         'url':'http://www.tangowithdjango.com/'} ]
+    
+    other_pages = [
+        {'title':'Bottle',
+         'url':'http://bottlepy.org/docs/dev/'},
+        {'title':'Flask',
+         'url':'http://flask.pocoo.org'} ]
+    
+    cats = {'Python': {'pages': python_pages},
+            'Django': {'pages': django_pages},
+            'Other Frameworks': {'pages': other_pages} }
+    
+    # Cats dictionary
+    # loop through all the pages
+    for cat, cat_data in cats.items():
+        c = add_cat(cat, views=cat_data['views'], likes=cat_data['likes'])
+        for p in cat_data['pages']:
+            add_page(c, p['title'], p['url'])
+    
+    for c in Category.objects.all():
+        for p in Page.objects.filter(category=c):
+            print(f'- {c}: {p}')
+
+def add_page(cat, title, url, views=0):
+    # get_or_create() -->  creating model instances in the population script above
+    #                 -->  returns a tuple of (object, created)
+    p = Page.objects.get_or_create(category=cat, title=title)[0]
+    p.url=url
+    p.views=views
+    p.save()
+    return p
+
+def add_cat(name, views=0, likes=0):
+    c = Category.objects.get_or_create(name=name)[0]
+    c.views = views
+    c.likes = likes
+    c.save()
+    return c
+
+# execution is not made unless this method is called: use "$ python module.py" in the command line
+if __name__ == '__main__':
+    print('Starting Rango population script...')
+    populate()
--- rango/admin.py
+++ rango/admin.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from rango.models import Category, Page
 
 # Register your models here.
+# these will be accessible via the web-based admin interface
 admin.site.register(Category)
 admin.site.register(Page)
